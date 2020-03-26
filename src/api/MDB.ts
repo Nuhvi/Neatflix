73fd21f24af2dc25b409dbd0c78f7a07b6b6c95c
nazeh <ar.nazeh@gmail.com>
2020-03-26 22:13:19 +0300

    test fetching data from MDB

--- src/api/MDB.ts
+++ src/api/MDB.ts
@@ -1,9 +1,19 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  params: {
-    api_key: process.env.MDB_API_KEY
-  }
-});
+export default (() => {
+  const config = {
+    baseURL: "https://api.themoviedb.org/3",
+    params: { api_key: process.env.VUE_APP_MDB_API_KEY }
+  };
+
+  const api = {
+    get: (url: string) => axios.get(url, config)
+  };
+
+  const discover = () => api.get("discover/movie");
+
+  return {
+    discover
+  };
+})();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <p v-for="movie in movies" :key="movie.id">{{ movie }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
+import MDB from "@/api/MDB";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld,
+    HelloWorld
   },
+  data: () => ({
+    movies: ""
+  }),
+  created() {
+    this.loadData();
+  },
+  methods: {
+    loadData: async function() {
+      const res = await MDB.discover();
+      this.movies = res.data.results;
+    }
+  }
 };
 </script>
