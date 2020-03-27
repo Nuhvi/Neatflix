8dbf97f55dbe60754c16213f228e6d8ab2341cb7
nazeh <ar.nazeh@gmail.com>
2020-03-27 13:55:31 +0300

    :lipstick: Update base styling

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "assets/base.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  min-height: 100%;
+  padding: 1em;
 }
 
 #nav {
