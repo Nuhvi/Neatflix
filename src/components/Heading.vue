63888cb1f36d2733a76d57b4581fbfed859f8b7d
nazeh <ar.nazeh@gmail.com>
2020-04-03 20:48:51 +0300

    Fix Heading font-size

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -5,7 +5,7 @@
     appear-to-class="appear-to"
     appear-active-class="appear-active"
   >
-    <h2>
+    <h2 class="heading">
       <slot />
     </h2>
   </transition>
@@ -20,30 +20,23 @@ export default {
 };
 </script>
 
-<style>
-h2 {
+<style lang="scss" scoped>
+.heading {
   text-transform: uppercase;
   font-family: "Raleway", "Montserrat";
   text-align: left;
   margin: 0;
   font-size: 3em;
   margin-bottom: -0.3em;
-  background: linear-gradient(
-    top,
-    rgba(255, 0, 29, 1) 0%,
-    rgba(254, 0, 79, 1) 100%
-  );
+  background: linear-gradient(top, rgba(255, 0, 29, 1) 0%, rgba(254, 0, 79, 1) 100%);
   background-clip: text;
   -webkit-text-fill-color: transparent;
-}
-@media screen and (min-width: $sm) {
-  h2 {
+
+  @media screen and (min-width: $sm) {
     font-size: 4em;
   }
-}
 
-@media screen and (min-width: $md) {
-  h2 {
+  @media screen and (min-width: $md) {
     font-size: 5em;
   }
 }
