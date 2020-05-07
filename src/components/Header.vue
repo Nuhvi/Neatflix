cdd4d9478f7387a2f316494a3d9635327754717d
nazeh <ar.nazeh@gmail.com>
2020-05-07 14:04:19 +0300

    Update #nav in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,17 @@
       <router-link to="/" class="logo">
         <img src="@/assets/Neatflix.svg" alt="Neatflix" />
       </router-link>
-      <div id="nav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/movies">Movies</router-link>
-        <router-link to="/tv">Series</router-link>
+
+      <div class="right">
+        <ul id="nav">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li><router-link to="/movies">Movies</router-link></li>
+          <li><router-link to="/tv">Series</router-link></li>
+        </ul>
+        <a class="placeholder">user</a>
       </div>
-      <a class="placeholder">user</a>
     </div>
   </header>
 </template>
@@ -26,20 +31,20 @@ header {
   position: absolute;
   z-index: 999;
   color: #9b9a9d;
-  border-bottom: 2px solid rgba(255, 255, 255, 0.05);
+  border-bottom: 2px solid rgba(255, 255, 255, 0.1);
 
   .container {
     z-index: -1;
 
     position: relative;
-    width: 100%;
+    width: 90%;
     max-width: $xl;
     margin: auto;
 
     padding: 2em 1em;
 
-    display: grid;
-    grid-template-columns: 160px auto 160px;
+    display: flex;
+    justify-content: space-between;
     align-items: center;
 
     .logo {
@@ -52,20 +57,52 @@ header {
       }
     }
 
-    #nav {
+    .right {
       display: flex;
-      justify-content: center;
-      align-items: center;
 
-      a {
-        color: inherit;
-        font-size: 1em;
-        padding: 0 0.4em;
+      #nav {
+        display: flex;
+        justify-content: end;
+        align-items: center;
 
-        &:hover,
-        &.router-link-exact-active {
-          color: rgb(207, 207, 207);
+        list-style: none;
+        margin: 0;
+        padding: 0;
+
+        li {
+          margin: 0;
+          padding: 0;
         }
+
+        a {
+          color: inherit;
+          font-size: 1em;
+          padding: 0 2em;
+
+          &:hover,
+          &.router-link-exact-active {
+            position: relative;
+            color: rgb(255, 255, 255);
+          }
+
+          &.router-link-exact-active {
+            &::after {
+              content: "";
+              position: absolute;
+              left: 50%;
+              bottom: -100%;
+              width: 5px;
+              height: 5px;
+              background: red;
+
+              border-radius: 50%;
+            }
+          }
+        }
+      }
+
+      .placeholder {
+        padding-left: 2em;
       }
     }
   }
--- src/router/index.ts
+++ src/router/index.ts
@@ -15,14 +15,6 @@ const routes = [
     path: "/movie/:id/*",
     name: "Movie",
     component: Movie
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
